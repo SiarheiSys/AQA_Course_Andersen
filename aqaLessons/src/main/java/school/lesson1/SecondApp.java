1aa64d0664ad3ce9c37cd3b1967a81aaf8b2ee42
SiarheiSys <sissergey@yandex.by>
2021-07-28 08:00:51 +0300

    Test 1

---
+++ aqaLessons/src/main/java/school/lesson1/SecondApp.java
@@ -0,0 +1,12 @@
+package school.lesson1;
+
+public class SecondApp {
+    public static void main(String[] args) {
+        int a;
+        int b;
+        a = 128;
+        System.out.println("a = " + a);
+        b = a / 2;
+        System.out.println("b = a / 2 = " + b);
+    }
+}
