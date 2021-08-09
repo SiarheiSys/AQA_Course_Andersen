3339d55a1e0dd59b0051e2e2809efd40a8b5a5ec
SiarheiSys <sissergey@yandex.by>
2021-08-09 23:27:28 +0300

    Homework has been done

--- aqaLessons/src/main/java/Lesson_4/Rectangle.java
+++ aqaLessons/src/main/java/Lesson_4/Rectangle.java
@@ -2,9 +2,9 @@ package Lesson_4;
 
 import Lesson_4.impl.Compute;
 
-public class Circle implements Compute {
-    private final double PI = 3.14;
-    private double radius;
+public class Rectangle implements Compute {
+    private double side1;
+    private double side2;
     private String fillColor;
     private String borderColor;
 
@@ -16,23 +16,28 @@ public class Circle implements Compute {
         return borderColor;
     }
 
-    public double getRadius() {
-        return radius;
+    public double getSide1() {
+        return side1;
     }
 
-    public Circle(double radius, String fillColor, String borderColor){
-        this.radius = radius;
+    public double getSide2() {
+        return side2;
+    }
+
+    public Rectangle(double side1, double side2, String fillColor, String borderColor) {
+        this.side1 = side1;
+        this.side2 = side2;
         this.fillColor = fillColor;
         this.borderColor = borderColor;
     }
 
     @Override
     public double perimetr() {
-        return 2 * PI * getRadius();
+        return 2 * (getSide1() + getSide2());
     }
 
     @Override
     public double square() {
-        return PI * getRadius() * getRadius();
+        return getSide1() * getSide2();
     }
 }
--- aqaLessons/src/main/java/Lesson_4/impl/Square.java
+++
@@ -1,7 +0,0 @@
-package Lesson_4.impl;
-
-import Lesson_4.Shape;
-
-public interface Square extends Perimetr {
-    double computeSquare(Shape shape);
-}
